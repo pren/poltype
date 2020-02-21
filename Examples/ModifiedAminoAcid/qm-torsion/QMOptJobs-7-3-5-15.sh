7439f98eaa3570fa9fa424ef0d3a54b0e4b5b7d3
bdw2292 <bdw2292@utexas.edu>
2020-02-21 13:36:08 -0600

    See README.VERSIONHISTORY

--- Examples/ModifiedAminoAcid/qm-torsion/QMOptJobs-7-3-5-15.sh
+++
@@ -1,2 +0,0 @@
-source /home/bdw2292/.poltype.bashrc
-GAUSS_SCRDIR=/scratch/bdw2292/Gau-ModifiedRes g09 ModifiedRes-opt-7-3-5-15-238.com
--- Examples/ModifiedAminoAcid/qm-torsion/QMSPJobs-7-3-5-15.sh
+++
@@ -1,2 +0,0 @@
-source /home/bdw2292/.poltype.bashrc
-GAUSS_SCRDIR=/scratch/bdw2292/Gau-ModifiedRes g09 ModifiedRes-sp-7-3-5-15-200.com
